9f3cf2c84bec0f02f27356317844851c0a8670a0
Miroslav Nemecek <Panda38@seznam.cz>
2025-06-23 09:37:46 +0200

    v0.01

---
+++ Pidipad/Games/Life/img/test.bat
@@ -0,0 +1,5 @@
+@echo off
+PidiPadImg imgintro.bmp imgintro.c ImgIntro 3
+PidiPadImg tile_empty.bmp tile_empty.c ImgTileEmpty 0
+PidiPadImg tile_cell.bmp tile_cell.c ImgTileCell 0
+PidiPadImg tile_core.bmp tile_core.c ImgTileCore 0
---
+++ Pidipad/Games/Life/img/tile_core.c
@@ -0,0 +1,9 @@
+#include "../include.h"
+
+// format: 1-bit pixel graphics
+// image width: 8 pixels
+// image height: 8 lines
+// image pitch: 1 bytes
+const u8 ImgTileCore[8] = {
+	0xFF, 0xFF, 0xE7, 0xC3, 0xC3, 0xE7, 0xFF, 0xFF, 
+};
